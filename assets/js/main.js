8561d337c7c5d136e57562960afbdd8da8652483
ucooling <mlb0903@163.com>
2017-02-15 17:32:20 +0800

    小试ES6中的Promise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,13 @@
+require('assets/css/style.scss');
 import * as func from 'components/hello.js';
 console.log("环境搭建成功");
+func.timeout(100).then((value)=> {
+    console.log(value);
+})
+
+func.getJSON('/post.json').then((json) => {
+    console.log('请求成功');
+}, (error)=>{
+    console.log('出错了', error);
+    console.dir(Promise);
+});
--- components/hello.js
+++ components/hello.js
@@ -1,3 +1,43 @@
-export function square(x) {
-	return x * x ;
+/**
+	* 实现一个简单的Promise
+	* Promise已经被ES6支持，所以不需要额外引入
+*/
+var timeout = function(ms) {
+	return new Promise((resolve, reject) => {
+		setTimeout(resolve, ms, 'done');
+	});
+};
+
+/**
+ * 使用Promise对象实现一个Ajax操作的例子
+ */
+
+var getJSON = function(url) {
+	var promise = new Promise((resolve, reject) => {
+		var client = new XMLHttpRequest();
+		client.open('GET', url);
+		client.onreadystatechange = handler;
+		client.responseType = 'json';
+		client.setRequestHeader("Accept", "application/json");
+		client.send();
+
+		function handler() {
+			if(this.readyState !==4) {
+				return;
+			}
+
+			if(this.state === 200) {
+				resolve(this.response);
+			}else {
+				reject(new Error(this.statusText));
+			}
+		}
+	});
+
+	return promise;
+}
+
+module.exports = {
+	timeout : timeout,
+    getJSON : getJSON
 }
